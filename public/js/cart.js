b6b7b2a4eb36b0835671495847c8a99b68f719a3
zzj9468 <976298632@qq.com>
2018-12-15 01:06:28 +0800

    购物车异步加载数据

---
+++ public/js/cart.js
@@ -0,0 +1,31 @@
+$(function(){
+    $.ajax({
+        url:'http://localhost:3000/cart',
+        type:'get'
+    }).then(res=>{
+        console.log(res);
+        var $tbody=$('.my_cart>.my_cart_main>table>tbody');
+        var html='';
+        for(var c of res){
+            html+=`<tr>
+            <td class="cf">
+                <input type="checkbox">
+                <a href="${c.href}"><img src="${c.pic}" alt=""></a>
+            </td>
+            <td><a href="${c.href}">${c.title}</a></td>
+            <td>¥<span>${c.price.toFixed(2)}</span>元</td>
+            <td class="cf">
+                <div class="cf">
+                    <a href="javascript:;">-</a>
+                    <input type="text" value="${c.count}">
+                    <a href="javascript:;">+</a>
+                </div>
+            </td>
+            <td>¥<span>${(c.count*c.price).toFixed(2)}</span>元</td>
+            <td><span>${c.jifen*c.count}</span></td>
+            <td><a href="">删除</a></td>
+        </tr>`;
+        }   
+        $tbody.html(html);
+    })
+})
