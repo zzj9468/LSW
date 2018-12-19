dfb33280fc86d54c4de2199ba740f0d56048fb40
zzj9468 <976298632@qq.com>
2018-12-20 00:49:16 +0800

    分页

--- public/js/search.js
+++ public/js/search.js
@@ -13,6 +13,10 @@ $(function(){
         $('.search>.search_main>.menu_info>.menu_list>div').addClass('hidden');
     })
 
+    var pno=0;
+    function loadPage(no=0){
+        pno=no;
+  
     //获取的数据
     if(location.search.indexOf('kwords')!==-1){
         var pno=0;
@@ -35,10 +39,37 @@ $(function(){
                 </a>
                 <p>${p.title}</p>
             <p>￥${p.prices.toFixed(2)}<del>￥${p.p_price.toFixed(2)}</del></p>
-            </li>
-`;
+            </li>`;
             }
             $('.search>.search_main>.container_info>div>ul').html(html);
+
+
+            var {pno,pageCount}=res;
+            var html=`<a href="javascript:;" ${pno==0?'disabled':''}>上一页</a>`;//html:47
+            for(var i=0;i<pageCount;i++){
+              html+=`<a href="javascript:;" ${pno==i?'active class="a_hover"':''}">${i+1}</a>
+              `;//html:49
+            }
+            html+=`<a href="javascript:;"  ${pno==pageCount-1?'disabled':''}>下一页</a>`;//html:51行
+            $(".search>.search_main>.container_info>.page").html(html);
+
         })
     }
+    }
+        loadPage();
+        $(".page").on(
+          "click",
+          "a:not(.disabled):not(.active)",
+          function(e){
+            e.preventDefault();
+            console.log(e.target);
+            var $a=$(this);
+            if($a.is(":first-child")){
+              loadPage(pno-1);
+            }else if($a.html()==="下一页"){
+              loadPage(pno+1);
+            }else{ /*?*/ }
+          }
+        )
+    
 })
