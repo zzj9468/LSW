b759b9325539678b90ac46986b0ddeb71103b374
zzj9468 <976298632@qq.com>
2018-12-20 21:45:32 +0800

    搜索页面完成

--- lsw/routes/search.js
+++ lsw/routes/search.js
@@ -17,18 +17,24 @@ router.get('/',(req,res)=>{
     if(req.query.pno!==undefined)
         output.pno=parseInt(req.query.pno);
         kwords=kwords.split(' ');
-        var arr=[];
+        kwords.forEach((val,i,arr)=>{
+            arr[i]=`%${val}%`;
+
+        })       
+         var arr=[];
         for(var f of kwords){   //不会改变原数组的值
             arr.push(` title like ? `)
         }
-        kwords.forEach((val,i,arr)=>{
-            arr[i]=`%${val}%`;
-        })
-       // console.log(kwords);
-         var where=" where "+arr.join(' and ');
-         var sql="select *,(select prev_price from lsw_details where pid=lid) as p_price  from lsw_index_product "+where;
-         //console.log(sql);
-         pool.query(sql,kwords,(err,result)=>{
+
+
+       //select *,(select prev_price from lsw_details where pid=lid) as p_price from lsw_index_product where title like '%葱%' and title like '%饼干%' or fid=(select fid from lsw_index_family where title like '%葱%' and title like '%饼干%')
+       //select *,(select prev_price from lsw_details where pid=lid) as p_price from lsw_index_product where title like '%饼干糕点%' or fid=(select fid from lsw_index_family where title like '%饼干糕点%')
+
+        var sql=`select *,(select prev_price from lsw_details where pid=lid) as p_price  from lsw_index_product where ${arr.join(' and ')}  or fid=(select fid from lsw_index_family where ${arr.join(' and ')} )`; 
+
+        var k=kwords.concat(kwords);
+        //console.log(k);
+         pool.query(sql,k,(err,result)=>{
              if(err) throw err;
              var count =result.length;
              var pageCount=Math.ceil(count/output.pageSize);
--- public/js/search.js
+++ public/js/search.js
@@ -16,10 +16,10 @@ $(function(){
     var pno=0;
     function loadPage(no=0){
         pno=no;
-  
+       // console.log(pno);
     //获取的数据
     if(location.search.indexOf('kwords')!==-1){
-        var pno=0;
+        
         var kwords=location.search.split('=')[1];
         $.ajax({
             url:'http://127.0.0.1:3000/search',
@@ -29,11 +29,12 @@ $(function(){
         })
         .then(res=>{
             console.log(res);
+            if(res.count>0){
             var {products}=res;
             var html='';
             for(var p of products){
                 html+=`<li>
-                <a href="${p.href}">
+                <a href="${p.hrefs}">
                     <img src="${p.imgs}" alt="${p.subtitle}">
                     <span></span>
                 </a>
@@ -41,35 +42,48 @@ $(function(){
             <p>￥${p.prices.toFixed(2)}<del>￥${p.p_price.toFixed(2)}</del></p>
             </li>`;
             }
-            $('.search>.search_main>.container_info>div>ul').html(html);
+           $('.search>.search_main>.container_info>div>ul').html(html);
 
 
             var {pno,pageCount}=res;
-            var html=`<a href="javascript:;" ${pno==0?'disabled':''}>上一页</a>`;//html:47
+            var html=`<a href="javascript:;" ${pno==0?'style="pointer-events:none;opacity:0.6"':''}>上一页</a>`;//html:47
             for(var i=0;i<pageCount;i++){
               html+=`<a href="javascript:;" ${pno==i?'active class="a_hover"':''}">${i+1}</a>
               `;//html:49
             }
-            html+=`<a href="javascript:;"  ${pno==pageCount-1?'disabled':''}>下一页</a>`;//html:51行
+            html+=`<a href="javascript:;"  ${pno==pageCount-1?'style="pointer-events:none;opacity:0.6"':''}>下一页</a>`;//html:51行
             $(".search>.search_main>.container_info>.page").html(html);
 
-        })
+        }else{
+            $('.err').removeClass('hidden');
+            page.addClass('hidden');
+            console.log($('.logo>.logo_main>ul>li>input').val());
+            $('.err>div>p>span').html($('.logo>.logo_main>ul>li>input').val());
+    
+        }
+    
+    })
+   
     }
     }
         loadPage();
-        $(".page").on(
+         var page=$(".page").on(
           "click",
           "a:not(.disabled):not(.active)",
           function(e){
             e.preventDefault();
-            console.log(e.target);
+            //console.log(e.target);
             var $a=$(this);
+            //console.log($a.is(":first-child"));
             if($a.is(":first-child")){
               loadPage(pno-1);
             }else if($a.html()==="下一页"){
               loadPage(pno+1);
-            }else{ /*?*/ }
+            }else{ 
+                console.log($a.html()-1);
+                loadPage(pno=$a.html()-1)
+             }
           }
         )
-    
+
 })
